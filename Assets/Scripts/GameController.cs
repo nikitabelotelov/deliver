e79f973c6b2cc6b766cc689597b167c60c8994e5
nikitabelotelov <nikbelotelov@gmail.com>
2020-06-26 18:17:58 +0300

    add gamecontroller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text timerText;
+    private GameModel gameModel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameModel = new GameModel(15000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var state = gameModel.GetState();
+        Debug.Log(state);
+        if(state == GameModel.States.PathBuild || state == GameModel.States.PathGo)
+        {
+            var time = gameModel.count(Mathf.FloorToInt(Time.deltaTime * 1000));
+            timerText.text = FormateTime(time);
+        } else if(state == GameModel.States.GameOver)
+        {
+            // TODO
+        }
+    }
+
+    private string FormateTime(int timeRaw)
+    {
+        string seconds = (timeRaw / 1000).ToString();
+        string mlsec = ((timeRaw % 1000) / 10).ToString();
+        return seconds + ':' + mlsec;
+    }
+
+    public void Click()
+    {
+        if(gameModel.GetState() == GameModel.States.PathGo) {
+            gameModel.NewOrder();
+        }
+    }
+
+    public void PathGo()
+    {
+        if (gameModel.GetState() == GameModel.States.PathBuild)
+        {
+            gameModel.StartPathGo();
+        }
+    }
+}
---
+++ Assets/Scripts/Helpers/GameModel.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class GameModel : ScriptableObject
+{
+    private int buildModeMultiplier = 3;
+    private int timeBonus = 10000;
+
+    public GameModel(int initTimer)
+    {
+        SetTimer(initTimer);
+    }
+
+    public enum States
+    {
+        PathBuild,
+        PathGo,
+        GameOver,
+    }
+
+    private static States state = States.PathBuild;
+    private static int gameTimer;
+
+    // 2000ms == 2 seconds
+    private static void SetTimer(int initialTimer)
+    {
+        gameTimer = initialTimer;
+    }
+
+    public void StartPathBuild()
+    {
+        state = States.PathBuild;
+    }
+
+    public void StartPathGo()
+    {
+        if (state != States.PathBuild)
+        {
+            return;
+        }
+        state = States.PathGo;
+    }
+
+    public States GetState()
+    {
+        return state;
+    }
+
+    public int count(int delay)
+    {
+        if (gameTimer <= 0)
+        {
+            state = States.GameOver;
+        }
+        if (state == States.PathBuild)
+        {
+            gameTimer -= Mathf.CeilToInt(delay / buildModeMultiplier);
+        }
+        else
+        {
+            gameTimer -= delay;
+        }
+        return gameTimer;
+    }
+
+    public void NewOrder()
+    {
+        state = States.PathBuild;
+        gameTimer += timeBonus;
+    }
+}
