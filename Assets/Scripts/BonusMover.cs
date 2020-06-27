d73ab5183caca40fe1bb1a3b91fb3ecac41205b3
ekateri9a <ekateri9a@yandex.ru>
2020-06-27 14:10:32 +0300

    Bonuses

---
+++ Assets/Scripts/BonusMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusMover : MonoBehaviour
+{
+    public float Speed = 0.1f;
+    public float exitAfterSeconds = 10f; // how long to exist in the world
+	private float targetTime;
+	void Start ()
+	{
+		// set the targetTime to be the current time + exitAfterSeconds seconds
+		targetTime = Time.time + exitAfterSeconds;
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= targetTime)
+			Destroy (gameObject);
+        
+        gameObject.transform.Translate(Vector3.right * Speed * Time.deltaTime);
+    }
+
+    void OnMouseDown()
+    {
+        // Destroy the gameObject after clicking on it
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnBonuses.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBonuses : MonoBehaviour
+{
+    public float secondsBetweenSpawning = 0.1f;
+	public float xMinRange = -25.0f;
+	public float xMaxRange = 25.0f;
+	public float yMinRange = 8.0f;
+	public float yMaxRange = 25.0f;
+	public GameObject[] spawnObjects; // what prefabs to spawn
+
+	private float nextSpawnTime;
+
+	void Start ()
+	{
+		// determine when to spawn the next object
+		nextSpawnTime = Time.time+secondsBetweenSpawning;
+	}
+	
+	void Update ()
+	{
+		// add exit if the game is over
+		// if()
+
+		if (Time.time  >= nextSpawnTime) {
+			MakeThingToSpawn ();
+			nextSpawnTime = Time.time+secondsBetweenSpawning;
+		}	
+	}
+
+	void MakeThingToSpawn ()
+	{
+		Vector3 spawnPosition;
+
+		spawnPosition.x = Random.Range (xMinRange, xMaxRange);
+		spawnPosition.y = Random.Range (yMinRange, yMaxRange);
+        spawnPosition.z = 0;
+
+		int objectToSpawn = Random.Range (0, spawnObjects.Length);
+
+		GameObject spawnedObject = Instantiate (spawnObjects [objectToSpawn], spawnPosition, Quaternion.Euler(0, 0, 0)) as GameObject;
+
+		spawnedObject.transform.parent = gameObject.transform;
+	}
+}
