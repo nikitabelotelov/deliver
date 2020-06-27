d5ff35297e30e4dd3e8e49fada9d4ad7ea3713b0
nikitabelotelov <nikbelotelov@gmail.com>
2020-06-27 18:01:42 +0300

    fixes

--- Assets/Scripts/BackgroundHelper.cs
+++ Assets/Scripts/BackgroundHelper.cs
@@ -2,30 +2,26 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class BackgroundHelper : MonoBehaviour {
+public class BackgroundHelper : MonoBehaviour
+{
 
     public float speed = 0;
     float pos = 0;
     private RawImage image;
-    
-	void Start () {
+
+    void Start()
+    {
         image = GetComponent<RawImage>();
-        
-	
-	}
-	
-	
-	 void Update () {
+    }
 
 
+    void FixedUpdate()
+    {
         pos += speed;
         if (pos > 1.0F)
+        {
             pos -= 1.0F;
-
+        }
         image.uvRect = new Rect(pos, 0, 1, 1);
-
-
-    }   
-
-
+    }
 }
